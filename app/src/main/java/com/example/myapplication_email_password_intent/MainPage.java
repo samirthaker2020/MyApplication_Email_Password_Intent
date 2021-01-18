d8caf3bf309f85b3d320b3d12966e96cbec66810
samirthaker2020 <46947688+samirthaker2020@users.noreply.github.com>
2021-01-18 00:42:46 -0500

    added new activity

---
+++ app/src/main/java/com/example/myapplication_email_password_intent/MainPage.java
@@ -0,0 +1,14 @@
+package com.example.myapplication_email_password_intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+    }
+}
