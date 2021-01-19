d0788ba65666416ae451e7db3f24f00a4e6834ff
samirthaker2020 <46947688+samirthaker2020@users.noreply.github.com>
2021-01-18 19:07:55 -0500

    applied button click event, added navigation intent , prompt alert dialog

--- app/src/main/java/com/example/myapplication_email_password_intent/MainPage.java
+++ app/src/main/java/com/example/myapplication_email_password_intent/MainPage.java
@@ -3,12 +3,19 @@ package com.example.myapplication_email_password_intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainPage extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+
+
+
     }
 }
